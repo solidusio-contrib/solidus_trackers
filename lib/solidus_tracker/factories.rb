662b198a3e9d1acf2150988fdfe3f01ad2f08622
Mike Conlin <mikeyconlin@gmail.com>
2016-09-07 10:22:10 -0700

    Add factory for tracker

--- lib/solidus_tracker/factories.rb
+++ lib/solidus_tracker/factories.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
-  # Define your Spree extensions Factories within this file to enable applications, and other extensions to use and override them.
-  #
-  # Example adding this to your spec_helper will load these Factories for use:
-  # require 'solidus_tracker/factories'
+  factory :tracker, class: Spree::Tracker do
+    analytics_id 'A100'
+    active true
+  end
 end
