dcfa714b6ebed50610f12c39cd2e86433a843753
Mike Conlin <mikeyconlin@gmail.com>
2016-09-13 10:14:14 -0700

    Google anaylytics partial

---
+++ app/overrides/decorate_spree_application.rb
@@ -0,0 +1,7 @@
+Deface::Override.new(
+  virtual_path: "spree/layouts/spree_application",
+  name: "google_analytics_partial",
+  insert_bottom: "[data-hook='body']",
+  disabled: false,
+  partial: "spree/shared/google_analytics"
+)
