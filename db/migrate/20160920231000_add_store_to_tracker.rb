ed5732c6108ea98ecf65a2359115823f0ceb9e87
Mike Conlin <mikeyconlin@gmail.com>
2016-09-20 16:30:24 -0700

    Add store column to trackers

---
+++ db/migrate/20160920231000_add_store_to_tracker.rb
@@ -0,0 +1,17 @@
+class AddStoreToTracker < ActiveRecord::Migration
+  def self.up
+    if data_source_exists?('spree_trackers')
+      change_table :spree_trackers do |t|
+        t.references :store
+      end
+    end
+  end
+
+  def self.down
+    if data_source_exists?('spree_trackers')
+      change_table :spree_trackers do |t|
+        t.remove :store_id
+      end
+    end
+  end
+end
