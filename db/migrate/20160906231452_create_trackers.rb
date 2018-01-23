161856058c8987f3d2b365effa57ced24322872f
JD Guzman <jdguzman@mac.com>
2018-01-23 02:06:52 +0000

    add 5.1 as a migration version

--- db/migrate/20160906231452_create_trackers.rb
+++ db/migrate/20160906231452_create_trackers.rb
@@ -1,4 +1,4 @@
-class CreateTrackers < SolidusSupport::Migration[4.2][5.0]
+class CreateTrackers < SolidusSupport::Migration[4.2][5.0][5.1]
   def up
     unless data_source_exists?("spree_trackers")
       create_table :spree_trackers do |t|
--- db/migrate/20160920231000_add_store_to_tracker.rb
+++ db/migrate/20160920231000_add_store_to_tracker.rb
@@ -1,4 +1,4 @@
-class AddStoreToTracker < SolidusSupport::Migration[4.2]
+class AddStoreToTracker < SolidusSupport::Migration[4.2][5.0][5.1]
   def self.up
     if data_source_exists?('spree_trackers')
       change_table :spree_trackers do |t|
