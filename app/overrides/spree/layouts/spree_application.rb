17fc54a14cac14ff1cb631d25f62741b8c35f624
Jonathan Tapia <jonathan.tapia@magmalabs.io>
2018-03-15 14:10:00 -0600

    Update views and add necessary ones to support google manager tracking

--- app/overrides/spree/layouts/spree_application.rb
+++ app/overrides/spree/layouts/spree_application.rb
@@ -5,3 +5,11 @@ Deface::Override.new(
   disabled: false,
   partial: "spree/shared/google_analytics"
 )
+
+Deface::Override.new(
+  virtual_path: "spree/layouts/spree_application",
+  name: "google_manager_partial",
+  insert_bottom: "[data-hook='body']",
+  disabled: false,
+  partial: "spree/shared/google_manager"
+)
