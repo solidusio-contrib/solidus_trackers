ec17bc9f91a1794c44d7fb92d163c750ce6385ba
Jonathan Tapia <jonathan.tapia@magmalabs.io>
2018-11-20 16:27:02 -0600

    Specify tracking info adding service name

--- app/helpers/spree/admin/trackers_helper.rb
+++ app/helpers/spree/admin/trackers_helper.rb
@@ -2,7 +2,13 @@ module Spree
   module Admin
     module TrackersHelper
       def tracker_types
-        SolidusTrackers::Config[:types].split(' ')
+        SolidusTrackers::Config[:types].map do |type|
+          [type, type.downcase.tr(' ', '_')]
+        end
+      end
+
+      def display_tracker_type(tracker)
+        tracker.tracker_type.split('_').map(&:capitalize).join(' ')
       end
     end
   end
