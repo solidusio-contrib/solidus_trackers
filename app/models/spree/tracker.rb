b09c37de270c3b1abb59e8aedca38a4651fdd0ff
Jonathan Tapia <jonathan.tapia@magmalabs.io>
2018-11-20 16:21:08 -0600

    Add model scope by_type

--- app/models/spree/tracker.rb
+++ app/models/spree/tracker.rb
@@ -3,20 +3,37 @@ class Spree::Tracker < ActiveRecord::Base
 
   validates :tracker_type, presence: true
 
-  def self.current(store = nil, type = 'analytics')
+  def self.current(store = nil)
     return if !store
     if store.is_a?(Spree::Store)
-      Spree::Tracker.where(active: true, store_id: store, tracker_type: type)
+      Spree::Tracker.where(active: true, store_id: store)
     else
       # TODO: Remove in 2.0
       ActiveSupport::Deprecation.warn <<-EOS.squish, caller
         Calling Spree::Tracker.current with a string is DEPRECATED. Instead
         pass it an instance of Spree::Store.
       EOS
-      Spree::Tracker.where(active: true, tracker_type: type).joins(:store).where(
+      Spree::Tracker.where(active: true).joins(:store).where(
         "spree_stores.code = ? OR spree_stores.url LIKE ?",
         store, "%#{store}%"
       )
+    end
+  end
+
+  def self.by_type(store = nil, type = nil)
+    return if !store
+    if store.is_a?(Spree::Store)
+      Spree::Tracker.where(active: true, store_id: store, tracker_type: type).first
+    else
+      # TODO: Remove in 2.0
+      ActiveSupport::Deprecation.warn <<-EOS.squish, caller
+        Calling Spree::Tracker.by_type with a string is DEPRECATED. Instead
+        pass it an instance of Spree::Store.
+      EOS
+      Spree::Tracker.where(active: true, tracker_type: type).joins(:store).where(
+        "spree_stores.code = ? OR spree_stores.url LIKE ?",
+        store, "%#{store}%"
+      ).first
     end
   end
 end
