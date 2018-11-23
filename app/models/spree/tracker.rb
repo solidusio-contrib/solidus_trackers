a0499e412590b2f2ed0d2d82ecc719614dcd9894
Jonathan Tapia <jonathan.tapia@magmalabs.io>
2018-11-23 16:42:48 -0600

    Depracate self.current method

--- app/models/spree/tracker.rb
+++ app/models/spree/tracker.rb
@@ -6,7 +6,7 @@ class Spree::Tracker < ActiveRecord::Base
   def self.current(store = nil)
     return if !store
     if store.is_a?(Spree::Store)
-      Spree::Tracker.where(active: true, store_id: store)
+      Spree::Tracker.where(active: true, store_id: store).first
     else
       # TODO: Remove in 2.0
       ActiveSupport::Deprecation.warn <<-EOS.squish, caller
@@ -16,24 +16,18 @@ class Spree::Tracker < ActiveRecord::Base
       Spree::Tracker.where(active: true).joins(:store).where(
         "spree_stores.code = ? OR spree_stores.url LIKE ?",
         store, "%#{store}%"
-      )
-    end
-  end
-
-  def self.by_type(store = nil, type = nil)
-    return if !store
-    if store.is_a?(Spree::Store)
-      Spree::Tracker.where(active: true, store_id: store, tracker_type: type).first
-    else
-      # TODO: Remove in 2.0
-      ActiveSupport::Deprecation.warn <<-EOS.squish, caller
-        Calling Spree::Tracker.by_type with a string is DEPRECATED. Instead
-        pass it an instance of Spree::Store.
-      EOS
-      Spree::Tracker.where(active: true, tracker_type: type).joins(:store).where(
-        "spree_stores.code = ? OR spree_stores.url LIKE ?",
-        store, "%#{store}%"
       ).first
     end
+  end
+  class << self
+    deprecate current_tracker: :current, deprecator: Spree::Deprecation
+  end
+
+  def self.by_type(store: Spree::Store.default, type: 'google_analytics')
+    return if !store
+
+    Spree::Tracker.where(active: true,
+                         store_id: store,
+                         tracker_type: type).first
   end
 end
