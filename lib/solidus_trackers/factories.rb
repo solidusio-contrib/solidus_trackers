ccf172babf48a4df84a7eb0edb7d8a431c233d07
Jonathan Tapia <jonathan.tapia@magmalabs.io>
2018-12-05 13:43:06 -0600

    spec

--- lib/solidus_trackers/factories.rb
+++ lib/solidus_trackers/factories.rb
@@ -1,6 +1,8 @@
 FactoryBot.define do
   factory :tracker, class: Spree::Tracker do
+    name 'Google Analytics'
     analytics_id 'A100'
     active true
+    script '<script></script>'
   end
 end
--- spec/features/admin/configuration/analytics_tracker_spec.rb
+++ spec/features/admin/configuration/analytics_tracker_spec.rb
@@ -18,13 +18,15 @@ describe "Analytics Tracker", type: :feature do
 
     it "should have the right tabular values displayed" do
       within_row(1) do
-        expect(column_text(1)).to eq("A100")
-        expect(column_text(2)).to eq("Yes")
+        expect(column_text(1)).to eq("Google Analytics")
+        expect(column_text(2)).to eq("A100")
+        expect(column_text(3)).to eq("Yes")
       end
 
       within_row(2) do
-        expect(column_text(1)).to eq("A100")
-        expect(column_text(2)).to eq("Yes")
+        expect(column_text(1)).to eq("Google Analytics")
+        expect(column_text(2)).to eq("A100")
+        expect(column_text(3)).to eq("Yes")
       end
     end
   end
@@ -38,15 +40,18 @@ describe "Analytics Tracker", type: :feature do
 
     it "should be able to create a new analytics tracker" do
       click_link "admin_new_tracker_link"
+      fill_in "tracker_name", with: "Google Analytics"
       fill_in "tracker_analytics_id", with: "A100"
       option = first("#tracker_store_id option").text
       select option, from: "tracker_store_id"
+      fill_in "tracker_script", with: "<script></script>"
       click_button "Create"
 
       expect(page).to have_content("successfully created!")
       within_row(1) do
-        expect(column_text(1)).to eq("A100")
-        expect(column_text(2)).to eq("Yes")
+        expect(column_text(1)).to eq("Google Analytics")
+        expect(column_text(2)).to eq("A100")
+        expect(column_text(3)).to eq("Yes")
       end
     end
   end
